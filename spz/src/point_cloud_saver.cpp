799b1ab23b1153365239eba9885fc593b9614510
SebiGoebel <65459981+SebiGoebel@users.noreply.github.com>
2023-07-19 13:16:16 +0200

    Add files via upload

---
+++ spz/src/point_cloud_saver.cpp
@@ -0,0 +1,35 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/PointCloud2.h"
+
+#include "pcl_ros/point_cloud.h"
+#include "pcl/io/pcd_io.h"
+#include "ros/package.h"
+
+// Callback-Funktion, um die PointCloud2-Nachricht zu empfangen
+void pointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr &msg)
+{
+  // PointCloud2-Nachricht in pcl::PointCloud umwandeln
+  pcl::PointCloud<pcl::PointXYZ> pcl_pc;
+  pcl::fromROSMsg(*msg, pcl_pc);
+
+  // Pfad zur PointCloud-Datei
+  std::string file_path = ros::package::getPath("spz") + "/data/pointcloud.pcd";
+
+  // PointCloud speichern
+  pcl::PCDWriter writer;
+  writer.write<pcl::PointXYZ>(file_path, pcl_pc);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "point_cloud_saver");
+
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("/camera/depth/color/points", 1000, pointCloudCallback);
+
+  ros::spin();
+
+  return 0;
+}
