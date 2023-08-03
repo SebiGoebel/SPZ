bb922a9cdd46fe5f56f207c84679ee6ebcca2078
SebiGoebel <65459981+SebiGoebel@users.noreply.github.com>
2023-08-03 12:53:48 +0200

    Add files via upload

---
+++ poseEstimationPPF/build/CMakeFiles/poseEstimationPPF.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/poseEstimationPPF.dir/poseEstimationPPF.cpp.o"
+  "poseEstimationPPF"
+  "poseEstimationPPF.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/poseEstimationPPF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
